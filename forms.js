7779ba63fcdbc45836b08421dbae6a5148431cdf
RaylonJMG <ryj2213@gmail.com>
2024-10-29 22:59:14 -0500

    added Sass files and notes

--- forms.js
+++ forms.js
@@ -2,10 +2,10 @@ function myProcessForm(event) {
     event.preventDefault(); //prevents the page from refreshing
     const form = event.target;
     const passwordInput = form[0];
-    const firstnameInput = form[1];
+    const firstNameInput = form[1];
     const lastnameInput = form[2];
-    const firstname = firstnameInput.value;
-    output(`Your password is ${firstname}.<br>`);
+    const firstName = firstnameInput.value;
+    output(`Your password is ${firstName}.<br>`);
 }
 
 function processForm123(event) {
